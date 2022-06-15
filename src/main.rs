c1cb0b5e6ddaae35553c861b1fe5995894ddee2a
Andrei Litvin <andy314@gmail.com>
2022-06-15 13:28:05 -0400

    Parsing for matter data is available

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,93 @@
+use std::fmt::Debug;
 use std::time::Duration;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use btleplug::api::bleuuid::uuid_from_u16;
+use byteorder::{ByteOrder, LittleEndian};
 use log::{info, warn};
 
 use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter};
 use btleplug::platform::Manager;
 use tokio::time;
+
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+struct VendorId(u16);
+
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+struct ProductId(u16);
+
+impl Debug for ProductId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("ProductId(0x{:X})", self.0))
+    }
+}
+
+impl Debug for VendorId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("VendorId(0x{:X})", self.0))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+struct Discriminator(u16); // 12 bit max
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct MatterBleCommissionableData {
+    discriminator: Discriminator,
+    vendor_id: Option<VendorId>,
+    product_id: Option<ProductId>,
+}
+
+fn parse_advertising_data(data: &[u8]) -> Result<MatterBleCommissionableData> {
+    if data.len() < 8 {
+        return Err(anyhow!("Advertising data too short"));
+    }
+    // Advertising format:
+    //   - 1 byte:    matter op-code. 0x00 is 'Commissionable', the rest are undefined
+    //   - 2 byte LE: 4 bits version (0 is supported), 12 bits discriminator
+    //   - 2 byte LE: vendor id. 0 means undefined/missing
+    //   - 2 byte LE: product id. 0 means undefined/missing
+    //   - 1 byte:    flags. bit0 is 'additional data' flag, rest are reserved and MUST be 0
+    let opcode = data[0];
+
+    if opcode != 0x00 {
+        return Err(anyhow!(
+            "Unsupported opcode. Only Commissionable (0x00) is supported."
+        ));
+    }
+
+    let version_and_discriminator = LittleEndian::read_u16(&data[1..3]);
+    let vendor_id = LittleEndian::read_u16(&data[3..5]);
+    let product_id = LittleEndian::read_u16(&data[5..7]);
+
+    let version = (version_and_discriminator >> 12) & 0x0F;
+    let discriminator = Discriminator(version_and_discriminator & 0x0FFF);
+    let vendor_id = if vendor_id == 0 {
+        None
+    } else {
+        Some(VendorId(vendor_id))
+    };
+    let product_id = if product_id == 0 {
+        None
+    } else {
+        Some(ProductId(product_id))
+    };
+
+    if version != 0 {
+        return Err(anyhow!(
+            "Unsupported commissionable payload version: {}",
+            version
+        ));
+    }
+
+    // TODO: additional data?
+
+    return Ok(MatterBleCommissionableData {
+        discriminator,
+        vendor_id,
+        product_id,
+    });
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -27,10 +108,8 @@ async fn main() -> Result<()> {
     let matter_uuid = uuid_from_u16(0xFFF6);
     info!("UUID: {:?}", matter_uuid);
 
-    let scan_filter = ScanFilter::default();
     /*
-    let scan_filter = ScanFilter{services: vec![matter_uuid] });
-     */
+    let scan_filter = ScanFilter::default();
 
     adapter
         .start_scan(scan_filter)
@@ -38,6 +117,8 @@ async fn main() -> Result<()> {
         .expect("Can't scan BLE adapter for connected devices.");
 
     time::sleep(Duration::from_secs(2)).await;
+    adapter.stop_scan();
+   */
     let peripherals = adapter.peripherals().await?;
 
     info!("Found {} peripherals", peripherals.len());
@@ -56,41 +137,26 @@ async fn main() -> Result<()> {
         }
         let props = props.unwrap();
 
-        if !props.service_data.contains_key(&matter_uuid) {
-            warn!("{:?} Does not look like a matter device.", props.address);
-            continue;
-        }
-
-        info!("Peripheral {:?}: {:?}", props.address, props.service_data);
-        /*
-        if matches!(props.rssi, Some(level) if level < -60) {
-            warn!("  RSSI too low.");
-            continue;
-        }
-        */
-
-        let connected = peripheral.is_connected().await?;
-        if connected {
-            info!("  CONNECTED");
-        } else {
-            info!("  NOT CONNECTED. Connecting now.");
-            if let Err(err) = peripheral.connect().await {
-                eprintln!("   Failed to connect: {:?}", err);
+        let data = match props.service_data.get(&matter_uuid) {
+            None => {
+                warn!("{:?} Does not look like a matter device.", props.address);
                 continue;
             }
-        }
+            Some(data) => {
+                let data = parse_advertising_data(data.as_slice());
 
-        let props = peripheral.properties().await.unwrap().unwrap();
-        info!("  Local name {:?}", props.local_name);
+                if data.is_err() {
+                    warn!("Invalid matter data: {}", data.err().unwrap());
+                    continue;
+                }
 
-        peripheral.discover_services().await?;
-        for service in peripheral.services() {
-            info!("  SERVICE {:?}", service.uuid);
-        }
+                data.unwrap()
+            }
+        };
 
-        if connected {
-            peripheral.disconnect().await.expect("Disconnecting");
-        }
+        if !props.service_data.contains_key(&matter_uuid) {}
+
+        println!("Peripheral {:?}: {:?}", props.address, data);
     }
 
     Ok(())
