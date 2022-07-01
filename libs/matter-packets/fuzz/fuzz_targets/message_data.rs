c477180d8207767f0530083e5474d42a3a391b29
Andrei Litvin <andy314@gmail.com>
2022-07-01 14:41:15 -0400

    Update reading logic even more: it sees more normal to consume an array when reading data

--- libs/matter-packets/fuzz/fuzz_targets/message_data.rs
+++ libs/matter-packets/fuzz/fuzz_targets/message_data.rs
@@ -4,6 +4,7 @@ use libfuzzer_sys::fuzz_target;
 use matter_packets::*;
 
 fuzz_target!(|data: &[u8]| {
-    let data = LittleEndianReader::new_const(data);
-    MessageData::parse(data);
+    let mut data = Vec::from(data);
+    let mut data = data.as_mut_slice();
+    MessageHeader::parse(&mut data).ok();
 });
--- libs/matter-packets/src/lib.rs
+++ libs/matter-packets/src/lib.rs
@@ -132,6 +132,67 @@ impl SecurityFlags {
     }
 }
 
+/// Errors when reading endian-specific data
+#[derive(Debug, PartialEq)]
+pub enum EndianReadError {
+    InsufficientData,
+}
+
+impl Display for EndianReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EndianReadError::InsufficientData => f.write_str("Insufficient data"),
+        }
+    }
+}
+
+impl Error for EndianReadError {}
+
+pub trait BytesConsumer {
+    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError>;
+}
+
+impl BytesConsumer for &[u8] {
+    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError> {
+        self.take(..count)
+            .ok_or_else(|| EndianReadError::InsufficientData)
+    }
+}
+
+impl BytesConsumer for &mut [u8] {
+    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError> {
+        match self.take_mut(..count) {
+            Some(data) => Ok(data),
+            None => Err(EndianReadError::InsufficientData),
+        }
+    }
+}
+
+pub trait LittleEndianReader {
+    fn read_le_u8(&mut self) -> core::result::Result<u8, EndianReadError>;
+    fn read_le_u16(&mut self) -> core::result::Result<u16, EndianReadError>;
+    fn read_le_u32(&mut self) -> core::result::Result<u32, EndianReadError>;
+    fn read_le_u64(&mut self) -> core::result::Result<u64, EndianReadError>;
+}
+
+impl<T: BytesConsumer> LittleEndianReader for T {
+    fn read_le_u8(&mut self) -> core::result::Result<u8, EndianReadError> {
+        Ok(self.consume(1)?[0])
+    }
+
+    fn read_le_u16(&mut self) -> core::result::Result<u16, EndianReadError> {
+        Ok(byteorder::LittleEndian::read_u16(self.consume(2)?))
+    }
+
+    fn read_le_u32(&mut self) -> core::result::Result<u32, EndianReadError> {
+        Ok(byteorder::LittleEndian::read_u32(self.consume(4)?))
+    }
+
+    fn read_le_u64(&mut self) -> core::result::Result<u64, EndianReadError> {
+        Ok(byteorder::LittleEndian::read_u64(self.consume(8)?))
+    }
+}
+
 /// Represents a message header with data contained in it
 ///
 /// # Binary layout
@@ -149,18 +210,15 @@ impl SecurityFlags {
 /// | `16 bytes`     | (Optional) Message Integrity Check (for all except unecrypted) |
 ///
 #[derive(Debug, Default, PartialEq)]
-pub struct MessageData<PayloadType> {
+pub struct MessageHeader {
     pub flags: SecurityFlags,
     pub session_id: u16,
     pub source: Option<NodeId>,
     pub destination: MessageDestination,
     pub counter: u32,
-    pub payload: PayloadType,
 }
 
-impl<PayloadType> MessageData<PayloadType>
-where LittleEndianReader<PayloadType>: BytesConsumer
-{
+impl MessageHeader {
     /// Parses a given buffer and interprets it as a MATTER message.
     ///
     /// Examples:
@@ -169,25 +227,31 @@ where LittleEndianReader<PayloadType>: BytesConsumer
     /// use matter_packets::*;
     ///
     /// // invalid messages are rejected
-    /// assert!(MessageData::parse(LittleEndianReader::new_const(&[])).is_err()); // too short
-    /// assert!(MessageData::parse(LittleEndianReader::new_const(&[0, 0, 0])).is_err()); // too short
-    /// assert!(MessageData::parse(LittleEndianReader::new_const(&[0x11, 0, 0, 0, 0, 0, 0, 0])).is_err()); // invalid version
+    /// let mut data: &[u8] = &[]; // too short
+    /// assert!(MessageHeader::parse(&mut data).is_err()); // too short
     ///
-    /// let data = &[
+    /// let mut data: &[u8] = &[0, 0, 0]; // too short
+    /// assert!(MessageHeader::parse(&mut data).is_err()); // too short
+    ///
+    /// let mut data: &[u8] = &[0x11, 0, 0, 0, 0, 0, 0, 0, 0]; // invalid version
+    /// assert!(MessageHeader::parse(&mut data).is_err());
+    ///
+    /// let mut data: &[u8] = &[
     ///   0x00,                   // flags: none set
     ///   0x34, 0x12,             // session id: 0x1234
     ///   0x00,                   // security flags
     ///   0x00, 0x00, 0x00, 0x00, // counter
+    ///   0xaa, 0xbb, 0xcc        // payload
     /// ];
-    /// let data = LittleEndianReader::new_const(data);
-    /// let data = MessageData::parse(data).unwrap();
+    /// let parsed = MessageHeader::parse(&mut data).unwrap();
     ///
-    /// assert_eq!(data.session_id, 0x1234);
-    /// assert_eq!(data.source, None);
-    /// assert_eq!(data.destination, MessageDestination::None);
-    /// assert_eq!(data.flags.session_type().unwrap(), SessionType::Unicast);
+    /// assert_eq!(parsed.session_id, 0x1234);
+    /// assert_eq!(parsed.source, None);
+    /// assert_eq!(parsed.destination, MessageDestination::None);
+    /// assert_eq!(parsed.flags.session_type().unwrap(), SessionType::Unicast);
+    /// assert_eq!(data, &[0xaa, 0xbb, 0xcc]);
     ///
-    /// let data = &[
+    /// let mut data: &[u8] = &[
     ///   0x06,                   // flags: Source node id and destination group
     ///   0x33, 0x22,             // session id: 0x2233
     ///   0x01,                   // security flags
@@ -195,8 +259,7 @@ where LittleEndianReader<PayloadType>: BytesConsumer
     ///   0x12, 0x34, 0x56, 0x78, 0xaa, 0xbb, 0xcc, 0xdd,  // source node id
     ///   0xcd, 0xab,             // destination group id
     /// ];
-    /// let data = LittleEndianReader::new_const(data);
-    /// let data = MessageData::parse(data).unwrap();
+    /// let data = MessageHeader::parse(&mut data).unwrap();
     ///
     /// assert_eq!(data.session_id, 0x2233);
     /// assert_eq!(data.source, Some(NodeId(0xddccbbaa78563412)));
@@ -204,15 +267,14 @@ where LittleEndianReader<PayloadType>: BytesConsumer
     /// assert_eq!(data.counter, 1);
     /// assert_eq!(data.flags.session_type().unwrap(), SessionType::GroupMulticast);
     ///  
-    /// let data = &[
+    /// let mut data: &[u8] = &[
     ///   0x01,                   // flags: Destination node id
     ///   0x33, 0x22,             // session id: 0x2233
     ///   0x00,                   // security flags
     ///   0x45, 0x23, 0x01, 0x00, // counter
     ///   0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,  // destination node id
     /// ];
-    /// let data = LittleEndianReader::new_const(data);
-    /// let data = MessageData::parse(data).unwrap();
+    /// let data = MessageHeader::parse(&mut data).unwrap();
     ///
     /// assert_eq!(data.session_id, 0x2233);
     /// assert_eq!(data.source, None);
@@ -221,31 +283,31 @@ where LittleEndianReader<PayloadType>: BytesConsumer
     /// ```
     ///
     ///
-    pub fn parse(mut buffer: LittleEndianReader<PayloadType>) -> Result<MessageData<PayloadType>> {
-        let message_flags = buffer.read_u8()?;
+    pub fn parse(buffer: &mut impl LittleEndianReader) -> Result<MessageHeader> {
+        let message_flags = buffer.read_le_u8()?;
 
         if message_flags & FLAGS_VERSION_MASK != FLAGS_VERSION_V1 {
             return Err(anyhow!("Not a valid CHIP v1 message."));
         }
 
-        let session_id = buffer.read_u16()?; // session id
-        let flags = SecurityFlags::from_bits(buffer.read_u8()?)
+        let session_id = buffer.read_le_u16()?; // session id
+        let flags = SecurityFlags::from_bits(buffer.read_le_u8()?)
             .ok_or_else(|| anyhow!("Invalid security flags"))?;
 
         // this makes sure session flags are valid
         flags.session_type()?;
 
-        let counter = buffer.read_u32()?;
+        let counter = buffer.read_le_u32()?;
 
         let source = if message_flags & FLAGS_SOURCE_NODE_ID_SET != 0 {
-            Some(NodeId(buffer.read_u64()?))
+            Some(NodeId(buffer.read_le_u64()?))
         } else {
             None
         };
 
         let destination = match message_flags & FLAGS_DESTINATION_MASK {
-            FLAGS_DESTINATION_NODE => MessageDestination::Node(NodeId(buffer.read_u64()?)),
-            FLAGS_DESTINATION_GROUP => MessageDestination::Group(GroupId(buffer.read_u16()?)),
+            FLAGS_DESTINATION_NODE => MessageDestination::Node(NodeId(buffer.read_le_u64()?)),
+            FLAGS_DESTINATION_GROUP => MessageDestination::Group(GroupId(buffer.read_le_u16()?)),
             _ => MessageDestination::None,
         };
 
@@ -254,13 +316,12 @@ where LittleEndianReader<PayloadType>: BytesConsumer
         //   - grab payload
         //   - consider MIC
         //
-        Ok(MessageData {
+        Ok(MessageHeader {
             session_id,
             source,
             destination,
             flags,
             counter,
-            payload: buffer.rest(),
         })
     }
 }
@@ -275,100 +336,25 @@ where LittleEndianReader<PayloadType>: BytesConsumer
 // - ???: extensions (secured) - based on flag: length (u16) + data
 // - ???: payload
 
-#[derive(Debug, PartialEq)]
-pub enum EndianReadError {
-    InsufficientData,
-}
-
-impl Display for EndianReadError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            EndianReadError::InsufficientData => f.write_str("Insufficient data"),
-        }
-    }
-}
-
-impl Error for EndianReadError {}
-
-
-pub trait BytesConsumer {
-    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError>;
-}
-
-pub struct LittleEndianReader<T> {
-    data: T,
-}
-
-impl<'a> LittleEndianReader<&'a [u8]> {
-    pub fn new_const(data: &'a [u8]) -> Self {
-        Self {data}
-    }
-}
-impl<'a> LittleEndianReader<&'a mut [u8]> {
-    pub fn new_mut(data: &'a mut [u8]) -> Self {
-        Self {data}
-    }
-}
-
-impl<'a> BytesConsumer for LittleEndianReader<&'a [u8]> {
-    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError> {
-        self.data.take(..count).ok_or_else(|| EndianReadError::InsufficientData)
-    }
-}
-
-impl BytesConsumer for LittleEndianReader<&mut [u8]> {
-    fn consume(&mut self, count: usize) -> core::result::Result<&[u8], EndianReadError> {
-        match self.data.take_mut(..count) {
-            Some(data) => Ok(data),
-            None => Err(EndianReadError::InsufficientData),
-        }
-    }
-}
-
-impl<'a, T> LittleEndianReader<T> 
-where LittleEndianReader<T>: BytesConsumer
-{
-    pub fn read_u8(&mut self) -> core::result::Result<u8, EndianReadError> {
-        Ok(self.consume(1)?[0])
-    }
-
-    pub fn read_u16(&mut self) -> core::result::Result<u16, EndianReadError> {
-        Ok(byteorder::LittleEndian::read_u16(self.consume(2)?))
-    }
-
-    pub fn read_u32(&mut self) -> core::result::Result<u32, EndianReadError> {
-        Ok(byteorder::LittleEndian::read_u32(self.consume(4)?))
-    }
-
-    pub fn read_u64(&mut self) -> core::result::Result<u64, EndianReadError> {
-        Ok(byteorder::LittleEndian::read_u64(self.consume(8)?))
-    }
-
-    pub fn rest(self) -> T {
-        self.data
-    }
-}
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn const_parse_data() {
-        let data = &[
+        let mut data: &[u8]  = &[
             1, 0x11, 0x12, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
         ];
 
-        let mut reader = LittleEndianReader::new_const(data);
+        assert_eq!(data.read_le_u8(), Ok(1));
+        assert_eq!(data.read_le_u16(), Ok(0x1211));
+        assert_eq!(data.read_le_u64(), Ok(0x0807060504030201));
+        assert!(data.read_le_u8().is_err());
 
-        assert_eq!(reader.read_u8(), Ok(1));
-        assert_eq!(reader.read_u16(), Ok(0x1211));
-        assert_eq!(reader.read_u64(), Ok(0x0807060504030201));
-        assert!(reader.read_u8().is_err());
-
-        let mut reader = LittleEndianReader::new_const(data);
-        assert_eq!(reader.read_u32(), Ok(0x01121101));
-        assert_eq!(reader.rest(), &[0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
+        let mut data: &[u8]  = &[
+            1, 0x11, 0x12, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+        ];
+        assert_eq!(data.read_le_u32(), Ok(0x01121101));
+        assert_eq!(data, &[0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]);
     }
 }
